df8da51195d19da2f518cdafd1600ff201e39e1e
nara <parknara0213@gmail.com>
2024-07-20 20:41:33 +0900

    240720 채팅 버튼 추가(테스트)

--- src/Trip.tsx
+++ src/Trip.tsx
@@ -1,16 +1,20 @@
 import PlanLayout from "layouts/PlanLayout";
 import RootLayout from "layouts/RootLayout";
+import Chat from "pages/Auth/Chat/Chat";
 import Login from "pages/Auth/Login";
 import { Route, Routes } from "react-router-dom";
 
 const Trip = () => {
   return (
-    <Routes>
-      <Route path="/" element={<RootLayout />}>
-        <Route path="/login" element={<Login />} />
-      </Route>
-      <Route path="/planner" element={<PlanLayout />}></Route>
-    </Routes>
+    <>
+      <Chat />
+      <Routes>
+        <Route path="/" element={<RootLayout />}>
+          <Route path="/login" element={<Login />} />
+        </Route>
+        <Route path="/planner" element={<PlanLayout />}></Route>
+      </Routes>
+    </>
   );
 };
 
