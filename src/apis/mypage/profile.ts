c60c97b5fe945decca9d5da3f5e3fba72f85cd98
nara <parknara0213@gmail.com>
2024-07-22 22:50:57 +0900

    240722 비밀번호 확인 및 변경

--- src/apis/mypage/profile.ts
+++ src/apis/mypage/profile.ts
@@ -18,3 +18,67 @@ export const fetchProfileAPI = async () => {
   console.log(profile);
   return profile;
 };
+
+// 현재 비밀번호 확인 API 호출 함수
+export const checkPassordAPI = async (password: string) => {
+  try {
+    // 비밀번호 확인 요청을 서버에 보냄
+    const response = await axios.post(
+      `${baseURL}/mypage/profile/checkPassword`, // 비밀번호 확인 엔드포인트
+      {
+        password, // 요청 본문에 비밀번호 포함
+      },
+      {
+        headers: {
+          "Content-Type": "application/json", // 요청 본문 타입을 JSON으로 설정
+          Access: `${localStorage.getItem("access")}`, // 액세스 토큰 헤더 설정
+          Refresh: `${getCookie("refresh")}`, // 리프레시 토큰 헤더 설정
+        },
+        withCredentials: true, // 쿠키를 요청에 포함
+      }
+    );
+
+    return response; // 서버 응답 반환
+  } catch (err: any) {
+    // 에러 발생 시 처리
+    if (err.response.data.code === 2) {
+      // 비밀번호가 일치하지 않는 경우
+      window.alert("잘못된 비밀번호입니다."); // 사용자에게 오류 메시지 알림
+    }
+  }
+};
+
+// 비밀번호 변경 API 호출 함수
+export const updatePasswordAPI = async (password: string) => {
+  try {
+    // 비밀번호 변경 요청을 서버에 보냄
+    const response = await axios.post(
+      `${baseURL}/mypage/profile/passwordUpdate`, // 비밀번호 변경 엔드포인트
+      {
+        password, // 요청 본문에 새로운 비밀번호 포함
+      },
+      {
+        headers: {
+          "Content-Type": "application/json", // 요청 본문 타입을 JSON으로 설정
+          Access: `${localStorage.getItem("access")}`, // 액세스 토큰 헤더 설정
+          Refresh: `${getCookie("refresh")}`, // 리프레시 토큰 헤더 설정
+        },
+        withCredentials: true, // 쿠키를 요청에 포함
+      }
+    );
+
+    return response; // 서버 응답 반환
+  } catch (err: any) {
+    // 에러 발생 시 처리
+    if (err.response.data.code === 1) {
+      // 비밀번호가 없거나 잘못된 경우
+      window.alert("잘못된 비밀번호입니다."); // 사용자에게 오류 메시지 알림
+    } else if (err.response.data.code === 2) {
+      // 비밀번호 변경 중 다른 에러 발생 시 처리
+      window.alert(""); // 빈 알림 (어떤 메시지도 표시하지 않음)
+    } else if (err.response.data.code === 6) {
+      // 현재 비밀번호와 동일한 비밀번호를 입력한 경우
+      window.alert("현재 비밀번호와 동일한 비밀번호입니다."); // 사용자에게 오류 메시지 알림
+    }
+  }
+};
--- src/pages/mypage/profile/Profile.tsx
+++ src/pages/mypage/profile/Profile.tsx
@@ -1,24 +1,119 @@
 import { useEffect, useState } from "react";
 import "./profile.css";
-import { fetchProfileAPI } from "apis/mypage/profile";
+import {
+  checkPassordAPI, // 비밀번호 확인 API
+  fetchProfileAPI, // 프로필 정보 가져오기 API
+  updatePasswordAPI, // 비밀번호 업데이트 API
+} from "apis/mypage/profile";
 import { UserType } from "types/users";
 import { convertDataToDate } from "utilities/profile";
 
 const Profile = () => {
+  // 사용자 정보를 저장할 상태
   const [user, setUser] = useState<UserType>();
-  // 사용자 프로필 정보 얻기
+  // 비밀번호 입력 상태
+  const [password, setPassword] = useState(
+    `***********************************`
+  );
+  // 모달 열기 상태
+  const [openModal, setOpenModal] = useState(false);
+  // 비밀번호 입력 필드 비활성화 상태
+  const [disabled, setDisabled] = useState(true);
+
+  // 컴포넌트가 처음 렌더링될 때 사용자 프로필 정보를 가져옴
   useEffect(() => {
     fetchProfileAPI()
       .then((res) => {
         console.log(res.data);
-
         setUser(res.data);
       })
       .catch((err) => console.log(err));
   }, []);
 
+  // 모달창을 열기 위한 핸들러
+  const handleModal = () => {
+    // 비밀번호 수정 확인 메시지
+    if (!window.confirm(`비밀번호를 수정하시겠습니까?`)) {
+      return;
+    }
+    setOpenModal(true);
+  };
+
+  // 입력한 비밀번호를 서버에서 확인하는 함수
+  const checkPassword = () => {
+    checkPassordAPI(password)
+      .then((res) => {
+        console.log(res?.data);
+        if (res?.data.code === "ok") {
+          setPassword(""); // 비밀번호 입력 필드 초기화
+          setOpenModal(false); // 모달 닫기
+          setDisabled(false); // 비밀번호 입력 필드 활성화
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+
+  // 비밀번호 입력 필드의 값이 변경될 때 호출되는 핸들러
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const currentPassword = e.currentTarget.value;
+    console.log(currentPassword);
+
+    setPassword(currentPassword);
+  };
+
+  // 비밀번호를 변경하는 함수
+  const changePassword = () => {
+    const validation = ValidatePw(password);
+    if (!validation) {
+      window.alert("잘못된 형식의 비밀번호입니다. 다시 작성해주세요.");
+      return;
+    }
+
+    if (!window.confirm(`입력한 비밀번호로 변경하시겠습니까?`)) {
+      return;
+    }
+
+    // 비밀번호 업데이트 API 호출
+    updatePasswordAPI(password).then((res) => {
+      if (!res) {
+        return;
+      }
+
+      if (res.data.code === "ok") {
+        setDisabled(true); // 비밀번호 입력 필드 비활성화
+        setPassword(`***********************************`); // 비밀번호 입력 필드 초기화
+        window.alert("비밀번호가 업데이트 되었습니다.");
+      }
+    });
+  };
+
+  // 비밀번호 유효성 검사 함수
+  const ValidatePw = (value: string) => {
+    // 비밀번호 정규 표현식 (영문자, 숫자, 특수문자 포함, 길이 8-12자)
+    const pwRegex = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,12}$/;
+    const result = pwRegex.test(value);
+    console.log(result);
+    return result;
+  };
+
+  console.log(user);
+  console.log(password);
+
   return (
     <div className="mypage-profile">
+      {openModal && (
+        <div className="mypage-profile-modal">
+          <div className="mypage-profile-modal-container">
+            <div className="mypage-profile-modal-main">
+              <p>현재 비밀번호를 입력해주세요.</p>
+              <input type="password" onChange={(e) => handleChange(e)} />
+              <button type="button" onClick={checkPassword}>
+                확인
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
       <div className="mypage-profile-container">
         <table className="mypage-profile-table">
           <thead>
@@ -41,7 +136,7 @@ const Profile = () => {
                 />
               </td>
               <td className="mypage-profile-body-td" colSpan={2}>
-                <input type="text" defaultValue={user?.nickname} />
+                <input type="text" value={user?.nickname || ""} />
               </td>
             </tr>
             <tr className="mypage-profile-body-row">
@@ -49,7 +144,7 @@ const Profile = () => {
                 <textarea
                   name=""
                   id=""
-                  defaultValue={user?.intro}
+                  value={user?.intro || ""}
                   placeholder="소개글을 작성해주세요"
                 />
               </td>
@@ -57,22 +152,41 @@ const Profile = () => {
             <tr className="mypage-profile-body-row">
               <th className="mypage-profile-body-th">이름</th>
               <td className="mypage-profile-body-td" colSpan={2}>
-                <input type="text" defaultValue={user?.username} disabled />
+                <input
+                  type="text"
+                  value={user?.username || ""}
+                  readOnly
+                  disabled
+                />
               </td>
             </tr>
             <tr className="mypage-profile-body-row">
               <th className="mypage-profile-body-th">비밀번호</th>
               <td className="mypage-profile-body-td">
-                <input type="password" defaultValue={user?.password} disabled />
+                <input
+                  type="password"
+                  value={password}
+                  disabled={disabled}
+                  onChange={(e) => handleChange(e)}
+                />
               </td>
               <td>
-                <button>수정</button>
+                {disabled ? (
+                  <button onClick={handleModal}>수정</button>
+                ) : (
+                  <button onClick={changePassword}>변경</button>
+                )}
               </td>
             </tr>
             <tr className="mypage-profile-body-row">
               <th className="mypage-profile-body-th">이메일</th>
               <td className="mypage-profile-body-td" colSpan={2}>
-                <input type="text" defaultValue={user?.email} disabled />
+                <input
+                  type="text"
+                  value={user?.email || ""}
+                  readOnly
+                  disabled
+                />
               </td>
             </tr>
             <tr className="mypage-profile-body-row">
@@ -80,7 +194,8 @@ const Profile = () => {
               <td className="mypage-profile-body-td" colSpan={2}>
                 <input
                   type="text"
-                  defaultValue={user?.gender === "m" ? "남성" : "여성"}
+                  value={user?.gender === "m" ? "남성" : "여성"}
+                  readOnly
                   disabled
                 />
               </td>
@@ -90,7 +205,8 @@ const Profile = () => {
               <td className="mypage-profile-body-td" colSpan={2}>
                 <input
                   type="text"
-                  defaultValue={user ? convertDataToDate(user?.birth) : ""}
+                  value={user ? convertDataToDate(user?.birth) : ""}
+                  readOnly
                   disabled
                 />
               </td>
@@ -98,13 +214,23 @@ const Profile = () => {
             <tr className="mypage-profile-body-row">
               <th className="mypage-profile-body-th">가입일</th>
               <td className="mypage-profile-body-td" colSpan={2}>
-                <input type="text" defaultValue={user?.regdate} disabled />
+                <input
+                  type="text"
+                  value={user?.regdate || ""}
+                  readOnly
+                  disabled
+                />
               </td>
             </tr>
             <tr className="mypage-profile-body-row">
               <th className="mypage-profile-body-th">신고당한 횟수</th>
               <td className="mypage-profile-body-td" colSpan={2}>
-                <input type="text" defaultValue={user?.reportCount} disabled />
+                <input
+                  type="text"
+                  value={user?.reportCount || 0}
+                  readOnly
+                  disabled
+                />
               </td>
             </tr>
             <tr className="mypage-profile-body-row">
@@ -112,9 +238,8 @@ const Profile = () => {
               <td className="mypage-profile-body-td" colSpan={2}>
                 <input
                   type="text"
-                  defaultValue={
-                    user?.role === "ROLE_USER" ? "사용자" : "관리자"
-                  }
+                  value={user?.role === "ROLE_USER" ? "사용자" : "관리자"}
+                  readOnly
                   disabled
                 />
               </td>
